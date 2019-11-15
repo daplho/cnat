340c86ca2239a46380dbf1b982673a5ae7f5cb2e
daplho <nskirov@gmail.com>
2019-11-14 17:46:01 -0800

    added majority of files needed for controller

---
+++ cnat-client-go/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"flag"
+	"path/filepath"
+
+	kubeinfomers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+
+	clientset "github.com/daplho/cnat/cnat-client-go/pkg/generated/clientset/versioned"
+	informers "github.com/daplho/cnat/cnat-client-go/pkg/generated/informers/externalversions"
+)
+
+var (
+	masterURL  string
+	kubeconfig string
+)
+
+func main() {
+	flag.StringVar(&kubeconfig, "kubeconfig", defaultKubeconfig(), "Path to a kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+
+	klog.InitFlags(nil)
+
+	flag.Parse()
+
+	cfg, err := rest.InClusterConfig()
+	if err != nil {
+		cfg, err = clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+		if err != nil {
+			klog.Fatalf("Error building kubeconfig: %s", err.Error())
+		}
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	cnatClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building cnat clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Minute*10)
+	cnatInformerFactory := informers.NewSharedInformerFactory(cnatClient, time.Minute*10)
+
+	controller := NewController(kubeClient, cnatClient, cnatInformerFactory.Cnat().V1alpha1().Ats(), kubeInformerFactory.Core().V1().Pods())
+
+	// notice that there is no need torun Start methods in a separate goroutine
+	// (i.e. go kubeInformerFactory.Start(stopCh)). Start method is non-blocking
+	// and runs all registered informers in a dedicated goroutine.
+	kubeInformerFactory.Start(wait.NeverStop)
+	cnatInformerFactory.Start(wait.NeverStop)
+
+	if err = controller.Run(2, wait.NeverStop); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
+}
+
+func defaultKubeconfig() string {
+	fname := os.Getenv("KUBECONFIG")
+	if fname != "" {
+		return fname
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		klog.Warningf("failed to get home directory: %v", err)
+		return ""
+	}
+	return filepath.Join(home, ".kube", "config")
+}
